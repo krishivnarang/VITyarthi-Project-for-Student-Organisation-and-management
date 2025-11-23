785005093199182639e4e7be6cd559ce3de09079
krishivnarang <krishivnarang07@gmail.com>
2025-11-23 11:07:32 +0530

    Create DuplicateEnrollException.java

---
+++ DuplicateEnrollException.java
@@ -0,0 +1,6 @@
+package edu.ccrm.exception;
+public class DuplicateEnrollmentException extends Exception {
+    public DuplicateEnrollmentException(String message) {
+        super(message); }
+    public DuplicateEnrollmentException(String message, Throwable cause) {
+        super(message, cause); } }

3611c5dff0a7b840b9728c55f27ffd7580d6e98d
krishivnarang <krishivnarang07@gmail.com>
2025-11-23 10:25:28 +0530

    Create Semester.java

---
+++ Semester.java
@@ -0,0 +1,21 @@
+package edu.ccrm.domain;
+/**
+ * Enum for Semester with Constructors and Fields
+ * Demonstrates Enum Usage
+ */
+public enum Semester {
+    SPRING("Spring", 1),
+    SUMMER("Summer", 2),
+    FALL("Fall", 3);
+    private final String displayName;
+    private final int order;
+    Semester(String displayName, int order) {
+        this.displayName = displayName;
+        this.order = order;  }
+    public String getDisplayName() {
+        return displayName; }
+    public int getOrder() {
+        return order; }
+    @Override
+    public String toString() {
+        return displayName; } }
